3843b97c15d955442d805bca5343a48d9737cad4
Usama Al Zayan <oszayyan91@hotmail.com>
2020-07-20 20:34:14 +0300

    Add the rest of the interfaces

---
+++ src/fbrs/controller/AllEntriesController.java
@@ -0,0 +1,33 @@
+package fbrs.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class AllEntriesController {
+    public TableView table;
+    public TableColumn fromColumn;
+    public TableColumn dateCreateColumn;
+    public TableColumn dateUptateColumn;
+    public TableColumn toColumn;
+    public TableColumn typeColumn;
+    public TableColumn quantityColumn1;
+    public TableColumn note;
+    public Button backBtn;
+
+    public void back(ActionEvent actionEvent) {
+    }
+
+    public void fromComboBox(ActionEvent actionEvent) {
+    }
+
+    public void toComboBox(ActionEvent actionEvent) {
+    }
+
+    public void dateUptateComboBox(ActionEvent actionEvent) {
+    }
+
+    public void typeComboBox(ActionEvent actionEvent) {
+    }
+}
