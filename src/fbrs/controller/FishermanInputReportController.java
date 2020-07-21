c7e43dc3af9b8a728eadf59f76073cc552ab97b0
Usama Al Zayan <oszayyan91@hotmail.com>
2020-07-21 11:13:31 +0300

    Add the rest of the interfaces

---
+++ src/fbrs/controller/FishermanInputReportController.java
@@ -0,0 +1,28 @@
+package fbrs.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class FishermanInputReportController {
+    public TableColumn numColumn;
+    public TableColumn numberColumn;
+    public TableColumn priceColumn;
+    public TableColumn sellerColumn;
+    public TableColumn noteColumn;
+    public Button backBtn;
+    public TextField search;
+    public Label FishermanName;
+    public Label account;
+    public TextField number;
+    public TextField name;
+    public Button addBtn;
+    public TableView table;
+    public Label id;
+    public TextField price;
+
+    public void back(ActionEvent actionEvent) {
+    }
+
+    public void onAdd(ActionEvent actionEvent) {
+    }
+}
--- src/fbrs/controller/SellerProfileController.java
+++ src/fbrs/controller/SellerProfileController.java
@@ -1,8 +1,14 @@
 package fbrs.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 
 public class SellerProfileController {
+    public TextField id;
+    public TextField name;
+    public TextField phone;
+    public TextField phone1;
+
     public void onClickSave(ActionEvent actionEvent) {
     }
 }
